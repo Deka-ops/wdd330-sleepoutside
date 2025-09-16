885b0a258493ade8a5c84b991b4133f8cf123a0d
mirianukeje <ukejemirian@gmail.com>
2025-09-16 13:34:09 +0100

    Product page

--- src/js/product.js
+++ src/js/product.js
@@ -1,7 +1,12 @@
-import { getLocalStorage, setLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage, getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+// import { getLocalStorage, getParam } from ".utils.mjs";
+// import { setLocalStorage, getParam } from "./utils.mjs";
+// import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
+const productId = getParam("product");
+console.log(dataSource.findProductById(productId));
 
 function addProductToCart(product) {
   const cartItems = getLocalStorage("so-cart") || []; 
