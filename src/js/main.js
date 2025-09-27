6bf847842522234d4ff16963bc945b40a5f74b8e
mirianukeje <ukejemirian@gmail.com>
2025-09-27 17:56:50 +0100

    build fix

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
 // Import the required modules
-import ProductData from "./ProductData.mjs";  // Handles fetching and managing product data
-import ProductList from "./ProductList.mjs";  // Responsible for rendering the list of products
-import Alert from "./alert.js";               // Handles displaying alert messages from a JSON file
+import ProductData from "./ProductData.mjs"; // Handles fetching and managing product data
+import ProductList from "./ProductList.mjs"; // Responsible for rendering the list of products
+import Alert from "./alert.js"; // Handles displaying alert messages from a JSON file
 
 // Create a new ProductData instance for the "tents" category
 const dataSource = new ProductData("tents");
