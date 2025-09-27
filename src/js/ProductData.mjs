b05558ef4d25cdb8722de0f400f6542ae2e1370d
mirianukeje <ukejemirian@gmail.com>
2025-09-27 17:55:57 +0100

    dist build

--- src/js/ProductData.mjs
+++ src/js/ProductData.mjs
@@ -10,7 +10,6 @@ export default class ProductData {
   constructor(category) {
     this.category = category;
     this.path = `/json/${this.category}.json`;
-    // this.path = `../public/json/${this.category}.json`;
   }
   getData() {
     return fetch(this.path)
